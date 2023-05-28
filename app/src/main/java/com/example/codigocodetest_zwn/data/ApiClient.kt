f0ac54c9abcaf77ecb8b2c4b5509cf9e36c7d21e
Zaw Win Naung <amerizaw@gmail.com>
2023-05-28 16:37:01 +0630

    handle_failure_network_call

--- app/src/main/java/com/example/codigocodetest_zwn/data/ApiClient.kt
+++ app/src/main/java/com/example/codigocodetest_zwn/data/ApiClient.kt
@@ -4,13 +4,22 @@ import com.example.codigocodetest_zwn.data.api.RetrofitService
 import com.example.codigocodetest_zwn.model.PopularMovieResponse
 import com.example.codigocodetest_zwn.model.UpcomingMovieResponse
 import retrofit2.Response
+import java.lang.Exception
 
 class ApiClient(private val retrofitService: RetrofitService) {
-    suspend fun getPopularMovies(apiKey: String): Response<PopularMovieResponse> {
-        return retrofitService.getPopularMovies(apiKey)
+    suspend fun getPopularMovies(apiKey: String): ApiResponse<PopularMovieResponse> {
+        return safeApiCall { retrofitService.getPopularMovies(apiKey) }
     }
 
-    suspend fun getUpcomingMovies(apiKey: String): Response<UpcomingMovieResponse> {
-        return retrofitService.getUpcomingMovies(apiKey)
+    suspend fun getUpcomingMovies(apiKey: String): ApiResponse<UpcomingMovieResponse> {
+        return safeApiCall { retrofitService.getUpcomingMovies(apiKey) }
+    }
+
+    private inline fun <T> safeApiCall(apiCall: () -> Response<T>): ApiResponse<T> {
+        return try {
+            ApiResponse.success(apiCall.invoke())
+        } catch (e: Exception) {
+            ApiResponse.failure(e)
+        }
     }
 }
